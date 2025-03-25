f23efe80805e8cf301e0400908b87cb57def1965
puly garcia <pulygarcia09@gmail.com>
2025-03-25 15:07:51 -0300

    db connection, ready to start adding models

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,6 +1,17 @@
 import { Sequelize } from "sequelize";
 import dotenv from 'dotenv'
 
-export const db = async () => {
-    
-}
+dotenv.config(); //enable env variables
+
+const DB_URL = process.env.DB_URL;
+
+export const db = new Sequelize(DB_URL, {
+    dialect: "postgres",
+    dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false, // allow connections without strict validations
+        },
+    },
+    logging: false, // avoid sql logs in console
+})
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,18 @@
 import express from 'express' 
 import morgan from 'morgan'
+import {db} from './config/database';
 
+async function connectDB() {
+    try {
+        await db.authenticate();
+        db.sync()
+        console.log("Connected to PostgreSQL with Sequelize");
+      } catch (error) {
+        console.error("Error connecting db:", error);
+    }
+}
+
+connectDB();
 const app = express()
 
 app.use(morgan('dev'))
